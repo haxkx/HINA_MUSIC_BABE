ce7912ab6564c26d4026546dfc68e0bdb4e54faa
Tanjiro Kamado <120044797+haxkx@users.noreply.github.com>
2024-05-06 08:14:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/haxkx/HINA_MUSIC_BABE",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "haxkx")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
